e4bc105a0e9b7219c243b8ed9462fd65d7ce2d5e
WILL LOCKWOOD <WRLOCKWOOD1@GMAIL.COM>
2015-06-04 16:39:46 -0700

    Add names to user model and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   resources :comments
+
   devise_for :users
   resources :links do
     member do
-      put "like", to: "links#upvote"
+      put "like", to:    "links#upvote"
       put "dislike", to: "links#downvote"
     end
     resources :comments
   end
+  root "links#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root "links#index"
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
